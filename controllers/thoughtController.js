6a42fe18ed6dc4387a4287d769ac041718da933b
chris <chrisj21283@gmail.com>
2024-01-28 14:25:07 -0500

    add to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -11,20 +11,34 @@ module.exports = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+
   getSingleThought: async (req, res) => {
     try {
-     
+      const thoughtById = await thought.findOne({_id: req.params.thoughtId})
+          .select ( "-__v");
+        console.log(thoughtById)
+        if (!thoughtById) {
+          res.status(404).json({ error: 'thought not found' });
+          return;
+        }
+        res.json(thoughtById);
     } catch (error) {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+
   createThought: async (req, res) => {
     try {
-   
+   const newThought = await thought.create(req.body)
+   res.status(201).json(newThought)
     } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(400).json({ error: 'Internal Server Error' });
     }
   },
+
+
   updateThought: async (req, res) => {
     try {
     
@@ -32,13 +46,24 @@ module.exports = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+
   deleteThought: async (req, res) => {
     try {
-     
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+      const delThoughtById = await thought.findOne({_id: req.params.thoughtId});
+   console.log(delThoughtById)
+   if (!delThoughtById) {
+     res.status(404).json({ error: 'thought not found' });
+     return;
+   }
+   res.json(delThoughtById);
+  
+ } catch (error) {
+   res.status(500).json({ error: 'Internal Server Error' });
+ }
   },
+
+
   createReaction: async (req, res) => {
     try {
     
@@ -46,6 +71,8 @@ module.exports = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+
   deleteReaction: async (req, res) => {
     try {
      
